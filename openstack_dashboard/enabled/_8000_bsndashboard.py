d8a36bfada29080e101d1e5e025bedc8fa3ec7f1
Michael Xiong <michael.xiong@bigswitch.com>
2016-06-30 11:32:33 -0700

    moved new bsn dashboard out to horizon_bsn created enable files for new pluginw

--- openstack_dashboard/enabled/_8000_bsndashboard.py
+++ openstack_dashboard/enabled/_8000_bsndashboard.py
@@ -14,19 +14,18 @@
 
 # The slug of the dashboard to be added to HORIZON['dashboards']. Required.
 DASHBOARD = 'bsndashboard'
+
 # If set to True, this dashboard will be set as the default dashboard.
 DEFAULT = False
+
+DISABLED = False
 # A dictionary of exception classes to be added to HORIZON['exceptions'].
 ADD_EXCEPTIONS = {}
-# A list of applications to be added to INSTALLED_APPS.
-ADD_INSTALLED_APPS = ['openstack_dashboard.dashboards.bsndashboard']
 
 ADD_ANGULAR_MODULES = [
-    'horizon.dashboard.bsndashboard',
+    'horizon_bsn.bsndashboard',
 ]
 
+ADD_INSTALLED_APPS = ['horizon_bsn.bsndashboard']
+
 AUTO_DISCOVER_STATIC_FILES = True
-
-ADD_JS_FILES = []
-
-ADD_JS_SPEC_FILES = []
